38ed8688fd450e098d60c8d58fd0c23f4e521084
Pranam Shaw <vea5a.pranam05@gmail.com>
2025-08-28 12:17:55 +0530

    updated checkout-controller

--- backend/src/controllers/checkout-Controller.js
+++ backend/src/controllers/checkout-Controller.js
@@ -1,11 +1,28 @@
-const express = require('express')
+        const express = require('express')
+        const CheckOut = require('../models/checkOut.js')
 
-const checkOut = async (req,res)=>{
-    try {
-        const userId = req.user._id
-    } catch (error) {
-        
-    }
-}
+        const checkOut = async (req,res)=>{
+            const userId = req.user._id
+            const {address, PhoneNumber,PinCode, City, State } = req.body
+            try {
+                let checkOutInfo = await CheckOut.findOne({userId})
+                if(checkOutInfo){
+                    checkOutInfo.address = address;
+                    checkOutInfo.PhoneNumber = PhoneNumber;
+                    checkOutInfo.PinCode = PinCode;
+                    checkOutInfo.City = City;
+                    checkOutInfo.State = State;
+                    await checkOutInfo.save();
+                }else{ 
+                    checkOutInfo = await CheckOut.create({
+                        userId,address,PhoneNumber,PinCode,City,State
+                })
+                }
 
-module.exports = checkOut
+            res.status(200).json({checkOutInfo})
+            } catch (error) {
+                res.status(500).json({message:"Something went wrong"})
+            }
+        }
+
+        module.exports = checkOut
